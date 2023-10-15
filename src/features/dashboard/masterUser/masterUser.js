ab9c1e6682177e968a1f38cd201f4afbfcd855f0
AnangCihuy06 <nanangw250@gmail.com>
2023-10-15 15:22:21 +0700

    updt

--- src/features/dashboard/masterUser/masterUser.js
+++ src/features/dashboard/masterUser/masterUser.js
@@ -1100,6 +1100,9 @@ const MasterUser = () => {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0, x: '-12rem' }}
+              style={{
+                display: 'flex',
+              }}
             >
               <AnimatePresence>
                 <motion.Box
