5028332c8584e04b0fe3875053bc43cfda5e8836
AnangCihuy06 <nanangw250@gmail.com>
2023-10-24 13:40:08 +0700

    daa

--- src/features/dashboard/product/view.js
+++ src/features/dashboard/product/view.js
@@ -15,7 +15,7 @@ const GlobalModal = ({ isOpen, onClose, onConfirm, children }) => {
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>delete</ModalHeader>
+        <ModalHeader>View PDF</ModalHeader>
         <ModalCloseButton />
         <ModalBody>{children}</ModalBody>
         <ModalFooter>
